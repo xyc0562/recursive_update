8db0e3831e7ddb5f5c853d1706cda1bfb4ad8b2f
Yecheng Xu <xyc0562@gmail.com>
2016-08-12 16:40:08 +0800

    Added batch_create_update action

--- lib/recursive_update/batch_actions.rb
+++ lib/recursive_update/batch_actions.rb
@@ -18,12 +18,15 @@ module RecursiveUpdate
           when :batch_update, :single_update
             mapping_name = :update_mappings
             op_options = { allow_root_create: false, allow_root_update: true, transaction: false }
+          when :batch_create_update
+            mapping_name = :update_mappings
+            op_options = { allow_root_create: true, allow_root_update: true, transaction: false }
           else
             raise UnreachableStateError.new "#{name} is unknown!"
         end
         mappings = send mapping_name if (mappings.nil? || mappings.empty?) && respond_to?(mapping_name)
         case name
-          when :batch_create, :batch_update
+          when :batch_create, :batch_update, :batch_create_update
             method = :bulk_recursive_update
           when :single_create, :single_update
             method = :recursive_update
@@ -57,6 +60,10 @@ module RecursiveUpdate
 
       def batch_update(params, *args, &block)
         _batch_operation params, :batch_update, *args, &block
+      end
+
+      def batch_create_update(params, *args, &block)
+        _batch_operation params, :batch_create_update, *args, &block
       end
 
       def single_create(params, *args, &block)
